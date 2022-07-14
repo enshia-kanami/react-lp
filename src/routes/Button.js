3e42fb977b4e23226952a7b318aa395ed5a03ac7
higashimoto-kanami <>
2022-07-14 19:44:36 +0900

    レイアウト修正

--- src/routes/Button.js
+++ src/routes/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) => {
     return (
-        <button className="w-1/6 bg-orange-300 text-white rounded drop-shadow-md m-3 p-2" type="submit">{props.buttonType}</button>
+        <button className="w-1/5 bg-orange-300 text-white text-[12px] rounded drop-shadow-md m-3 p-2" type="submit">{props.buttonType}</button>
     )
 }
 export default Button;
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -40,7 +40,7 @@ function Contact() {
 
     return (
         <contact className="bg-slate-200">
-                <div className="rounded-lg w-3/5 my-14 mr-auto ml-auto shadow-xl bg-white p-3">
+                <div className="rounded-lg w-3/5 max-w-md my-14 mr-auto ml-auto shadow-xl bg-white p-3">
                     <h3 className="text-center m-6 text-4xl">Contact</h3>
                         <form onSubmit={(e) => handleSubmit(e)}>
                             <div className="mb-2 text-center">
@@ -55,11 +55,11 @@ function Contact() {
                             <p className="text-red-600 text-xs">{formErrors.userEmail}</p>
                             <div className="mb-2 text-center">
                                 <label className="items-start text-left  align-top w-3/12 inline-block">Contact:</label>
-                                <textarea className="bg-gray-100" placeholder="text" tepe="textarea"></textarea>
+                                <textarea className="bg-gray-100" placeholder="text" tepe="textarea" ></textarea>
                             </div>
                             <div className="md:items-center text-center">
                                 <Button
-                                buttonType={"Send request"}
+                                buttonType={"SendRequest"}
                                 />
                                 {Object.keys(formErrors).length === 0 && isSubmit && (
                                     <div className="text-green-600">ログインに成功しました</div>

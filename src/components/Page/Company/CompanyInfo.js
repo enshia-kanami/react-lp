659705c5483472e79ffea980546ece9a9751321a
higashimoto-kanami <>
2022-07-19 19:46:41 +0900

    会社概要追加

---
+++ src/components/Page/Company/CompanyInfo.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function CompanyInfo() {
+    return(
+        <div className="m-auto w-3/4 sm:w-full">
+            <h3 className="mt-14 mb-4 mr-6 text-3xl font-bold">Company Info</h3>
+                <p>texttexttexttexttexttexttexttext</p>
+                <Link to="" className="text-sky-400 pt-4">詳細を見る</Link>
+        </div>
+    )
+}
+export default CompanyInfo;
